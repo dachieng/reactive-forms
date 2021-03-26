3187c0f0d258638b8c675087cf4b0e7a2346422f
dachieng <oloodorcas99@gmail.com>
2021-03-26 22:07:06 +0300

    template driven forms validation and error messages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'template', component: TemplateDrivenFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
